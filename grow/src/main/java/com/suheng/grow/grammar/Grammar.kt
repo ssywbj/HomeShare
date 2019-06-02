875e27dbace80262c5e7cb1912faaa08df9519b9
ssywbj <1271829012@qq.com>
2019-06-03 01:05:32 +0800

    用单元测试试运行方法

--- grow/src/main/java/com/suheng/grow/grammar/Grammar.kt
+++ grow/src/main/java/com/suheng/grow/grammar/Grammar.kt
@@ -5,21 +5,41 @@ class Grammar {
     private var y: Int = 11
     private val items = listOf("kiwifruit", "apple", "banana")
 
+    fun assignment() {
+        //val：定义只读变量且只能赋值一次
+        val a: Int = 1 //定义Int变量并赋值
+        //a = 35; error: Val cannot be reassigned
+        val b = 2 //定义变量并赋值，省略Int，根据数值“2”自动推断出类型
+        val c: Int //如果只定义变量但未赋值，不能省略类型
+        c = 3 //明确赋值
+
+        //var：定义可变变量
+        var x = 45
+        x = 3
+        println("val: a = $a, b = $b, c = $c, π = $pi, var: x = $x, ++y = ${++y}")
+
+        //字符串
+        val s1 = "x is $x"
+        x = 56
+        val s2 = "${s1.replace("is", "was")}, but now is $x"
+        println("$s1; $s2")
+    }
+
     fun sum(a: Int, b: Int): Int { //定义函数：返回值类型为Int
         return a + b//可以不用写分号
     }
 
     fun sum(a: Int, b: Int, c: Int) = a + b + c //定义函数：表达式作为函数体，自动推断返回值类型（简写）
 
-    private fun printUnit(a: Int, b: Int): Unit { //定义函数：返回值类型为无意义的值
+    fun printUnit(a: Int, b: Int): Unit { //定义函数：返回值类型为无意义的值
         println("$a + $b = ${a + b}")
     }
 
     //定义函数：返回值类型为无意义的值，Unit可省且简写，“$对象”表示对对象引用，“{}”表示对象的运算，“()”表示数值的运算
-    private fun printUnit(a: Int, b: Int, c: Int) =
+    fun printUnit(a: Int, b: Int, c: Int) =
         println("a, $a + $b + $c = ${a + b + c}, " + { a + b } + ", " + (a + b))
 
-    private fun maxOf(a: Int, b: Float): Int { //if条件表达式
+    fun maxOf(a: Int, b: Float): Int { //if条件表达式
         if (a > b) {
             return a
         } else {
@@ -28,17 +48,17 @@ class Grammar {
         //等价：return if (a > b) a else b.toInt()
     }
 
-    private fun maxOf(a: Int, b: Double): Int { //if条件表达式
+    fun maxOf(a: Int, b: Double): Int { //if条件表达式
         return if (a > b) a else b.toInt()
     }
 
-    private fun maxOf(a: Int, b: Int) = if (a > b) a else b //if条件表达式
+    fun maxOf(a: Int, b: Int) = if (a > b) a else b //if条件表达式
 
     private fun parseInt(str: String): Int? { //当某个变量的值可以为null时，必须在声明处的类型后添加?来标识该引用可为空
         return str.toIntOrNull() //如果str的内容不是数字返回 null
     }
 
-    private fun printProduct(arg1: String, arg2: String) {
+    fun printProduct(arg1: String, arg2: String) {
         val x = parseInt(arg1)
         val y = parseInt(arg2)
         //println("$x * $y = ${x * y}") //直接使用'x * y'会导致编译错误，因为他们可能为null
@@ -73,12 +93,12 @@ class Grammar {
         return null
     }
 
-    private fun printLength(obj: Any) {
+    fun printLength(obj: Any) {
         println("'$obj' string length is ${getStringLength(obj) ?: "error, not a string"}")
     }
 
     //使用区间（range）:使用in运算符来检测某个数字是否在指定区间内
-    private fun describeIn(x: Int) {
+    fun describeIn(x: Int) {
         val y = 11
         if (x in 4..y + 1) { //..区间运算符
             println("$x in range(4, ${y + 1})")
@@ -90,7 +110,7 @@ class Grammar {
     }
 
     //when表达式
-    private fun chooseWhen(obj: Any): String =
+    fun chooseWhen(obj: Any): String =
         when (obj) {
             1 -> "One"
             "Hello" -> "Greeting"
@@ -100,7 +120,7 @@ class Grammar {
         }
 
     //for循环
-    private fun printForAndIteration() {
+    fun printForAndIteration() {
         for (item in items) { //集合迭代
             print("$item\t")
         }
@@ -131,7 +151,7 @@ class Grammar {
     }
 
     //while循环
-    private fun printWhile() {
+    fun printWhile() {
         var index = 0
         while (index < items.size) {
             println("while, item at $index is ${items[index++]}")
--- grow/src/test/java/com/suheng/grow/GrammarTest.kt
+++ grow/src/test/java/com/suheng/grow/GrammarTest.kt
@@ -16,10 +16,73 @@ class GrammarTest {
     fun finish() = println("unit test finish")
 
     @Test
+    fun testAssignment() {
+        Grammar().assignment()
+    }
+
+    @Test
     fun testSum() {
         val grammar = Grammar()
         println("sum two = " + grammar.sum(3, 6))
         println("sum three = ${grammar.sum(3, 6, 9)}")
     }
 
+    @Test
+    fun testPrintUnit() {
+        val grammar = Grammar()
+        grammar.printUnit(4, 2)
+        grammar.printUnit(4, 2, 90)
+    }
+
+    @Test
+    fun testMaxOf() {
+        val grammar = Grammar()
+        println("max 0f is ${grammar.maxOf(3, 8.6f)}")
+        println("max 0f is " + grammar.maxOf(5, 9.63))
+        println("max 0f is ${grammar.maxOf(13, 9)}")
+    }
+
+    @Test
+    fun testPrintProduct() {
+        val grammar = Grammar()
+        grammar.printProduct("34", "2")
+        grammar.printProduct("3E4", "8")
+        grammar.printProduct("34", "Rr")
+    }
+
+    @Test
+    fun testPrintLength() {
+        val grammar = Grammar()
+        grammar.printLength("suheng")
+        grammar.printLength(1000)
+        grammar.printLength(listOf(Any()))
+    }
+
+    @Test
+    fun testDescribeIn() {
+        val grammar = Grammar()
+        grammar.describeIn(3)
+        grammar.describeIn(4)
+        grammar.describeIn(7)
+        grammar.describeIn(12)
+        grammar.describeIn(13)
+    }
+
+    @Test
+    fun testChooseWhen() {
+        val grammar = Grammar()
+        println(grammar.chooseWhen(1))
+        println(grammar.chooseWhen("Hello"))
+        println(grammar.chooseWhen(1000L))
+        println(grammar.chooseWhen(2))
+        println(grammar.chooseWhen(2.9f))
+        println(grammar.chooseWhen("other"))
+    }
+
+    @Test
+    fun testForWhile() {
+        val grammar = Grammar()
+        grammar.printForAndIteration()
+        grammar.printWhile()
+    }
 }
